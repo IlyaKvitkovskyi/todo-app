05e7991195f9bafbb93e0f328e54f7577fe61141
Illia <ilya.kvitkovskyi@gmail.com>
2022-07-02 20:29:46 +0300

    added Tasks component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+
 import List from './components/List';
 import AddList from './components/AddList';
+import Tasks from './components/Tasks';
+
 import DB from './assets/db.json';
 
 function App() {
@@ -48,7 +51,9 @@ function App() {
         />
         <AddList onAdd={onAddList} colors={DB.colors} />
       </div>
-      <div className="todo__tasks"></div>
+      <div className="todo__tasks">
+        <Tasks />
+      </div>
     </div>
   );
 }
