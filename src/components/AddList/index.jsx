87ea02365707feb339c0c7a2bee636f1e54e6cb5
Illia <ilya.kvitkovskyi@gmail.com>
2022-07-02 16:20:58 +0300

    added onClose function

--- src/components/AddList/index.jsx
+++ src/components/AddList/index.jsx
@@ -9,6 +9,12 @@ const AddButtonList = ({ colors, onAdd }) => {
   const [selectedColor, selectColor] = useState(colors[0].id);
   const [inputValue, setInputValue] = useState('');
 
+  const onClose = () => {
+    setVisiblePopup(false);
+    setInputValue('');
+    selectColor(colors[0].id);
+  }
+
   const addList = () => {
     if (!inputValue) {
       alert('Введите название списка');
@@ -20,9 +26,7 @@ const AddButtonList = ({ colors, onAdd }) => {
       name: inputValue,
       color,
     });
-    setVisiblePopup(false);
-    setInputValue('');
-    selectColor(colors[0].id);
+    onClose();
   };
 
   return (
@@ -63,7 +67,7 @@ const AddButtonList = ({ colors, onAdd }) => {
       {visiblePopup && (
         <div className="add-list__popup">
           <img
-            onClick={() => setVisiblePopup(false)}
+            onClick={onClose}
             className="add-list__popup-close-btn"
             src={closeSvg}
             alt="Close button"
