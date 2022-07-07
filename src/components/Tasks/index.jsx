d229b1fd33ee4263151f05c13ea793524d89693e
Illia Kvitkovskyi <kvitkovskii97@gmail.com>
2022-07-07 18:44:45 +0300

    fix title updates

--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 
 import editSvg from '../../assets/img/edit.svg';
 
 import './Tasks.scss';
 
-const Tasks = ({ list }) => {
+const Tasks = ({ list, onEditTitle }) => {
+  const editTitle = () => {
+    const newTitle = window.prompt('Название списка', list.name);
+    if (newTitle) {
+      onEditTitle(list.id, newTitle);
+      axios
+        .patch('http://localhost:3001/lists/' + list.id, {
+          name: newTitle,
+        })
+        .catch(() => {
+          alert('Не удалось обновить название списка!');
+        });
+    }
+  };
+
   return (
     <div className="tasks">
       <h2 className="tasks__title">
         {list.name}
-        <img src={editSvg} alt="Edit Icon" />
+        <img onClick={editTitle} src={editSvg} alt="Edit Icon" />
       </h2>
 
       <div className="tasks__items">
